fa76409984057df2f142636c19a036f740364e76
ljl <2977224746@qq.com>
2026-01-12 21:29:33 +0800

    added a missed question in disc07

--- disc/disc07_OOP.py
+++ disc/disc07_OOP.py
@@ -168,3 +168,35 @@ class Counter:
 
     def spawn(self):
         return Counter(self)
+
+class MissDict:
+    """Has a dict, gets a list of values for an iterable of keys, and counts keys that are not in the dict.
+
+    >>> double = MissDict({1: 2, 2: 4, 3: 6, 5: 10})
+    >>> half = MissDict({2: 1.0, 3: 1.5, 4: 2.0})
+    >>> double.get([1, 3, 5, 2, 4])  # No value for key 4 (1 miss)
+    [2, 6, 10, 4]
+    >>> double.get([5, 4, 3, 0, 4])  # No value for keys 0 or either 4 (3 misses)
+    [10, 6]
+    >>> half.get([1, 3, 5, 2, 4])    # No value for keys 1 or 5 (2 misses)
+    [1.5, 1.0, 2.0]
+    >>> print(double)
+    4/6 of the misses
+    """
+    misses = Counter()
+    def __init__(self, d):
+        assert isinstance(d, dict)
+        self.d = d
+        self.misses = self.misses.spawn()
+
+    def get(self, keys):
+        result = []
+        for k in keys:
+            if k in self.d:
+                result.append(self.d[k])
+            else:
+                self.misses.inc()
+        return result
+
+    def __str__(self):
+        return f'{self.misses.count}/{self.misses.parent.count} of the misses'
